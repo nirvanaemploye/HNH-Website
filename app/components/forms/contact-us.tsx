b560570ad04ff1cea5c7cb77715dcd353f412d8b
nirvanaemploye <nirvanaemploye@gmail.com>
2025-03-26 15:02:42 +0530

    done

--- app/components/forms/contact-us.tsx
+++ app/components/forms/contact-us.tsx
@@ -2,14 +2,19 @@ import Image from 'next/image';
 import React from 'react'
 import { FaMapMarkerAlt } from 'react-icons/fa';
 import { FaEnvelope, FaPhone } from "react-icons/fa6";
+import { useTheme } from '../context/themeContext';
 
 const ContactUsForm = () => {
+  const { theme } = useTheme();
   return (
     <div
-      style={{
+      /* style={{
         boxShadow: "0px 25.42px 38.13px #666AF513,inset 0px 0px 10px #ffffff30",
+      }} */
+      style={{
+        boxShadow: "0px 35.42px 48.13px #666AF521,inset 0px 0px 10px #ffffff30",
       }}
-      className="bg-white/5 rounded-2xl sm:p-10 p-5 grid md:grid-cols-2 grid-cols-1 gap-24  "
+      className="dark:bg-white/5 bg-[#FFFFFF0D] rounded-2xl sm:p-10 p-5 grid md:grid-cols-2 grid-cols-1 gap-24"
     >
       <div>
         <h5 className="capitalize text-primary font-oswald font-semibold text-3xl">
@@ -20,18 +25,18 @@ const ContactUsForm = () => {
         </p>
         <form className="sm:mt-14 mt-7 sm:space-y-9 space-y-5">
           <input
-            className="w-full h-14 px-4 bg-[#262036] border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
+            className="w-full h-14 px-4 dark:bg-[#262036] bg-[#FFFFFF0D] border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
             placeholder="Enter your name"
             type="text"
           />
           <input
-            className="w-full h-14 px-4 bg-[#262036] border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
+            className="w-full h-14 px-4 dark:bg-[#262036] bg-[#FFFFFF0D] border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
             placeholder="Enter your email address"
             type="email"
           />
           <textarea
             rows={4}
-            className="bg-[#262036] w-full text-[#898686] p-4 border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
+            className="dark:bg-[#262036] bg-[#FFFFFF0D] w-full text-[#898686] p-4 border border-primary [box-shadow:0px_4px_20px_#8B68FF25] rounded-lg"
           >
             Go ahead, We are listening...
           </textarea>
