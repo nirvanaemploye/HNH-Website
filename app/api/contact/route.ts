f303d9fea705613b31504854bbee0ba2d0aca93d
nirvanaemploye <nirvanaemploye@gmail.com>
2025-03-28 17:41:43 +0530

    added some functionality1

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
+
 export async function POST(req: Request) {
   try {
     const { name, email, message } = await req.json();
