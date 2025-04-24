d4e4c5897a57f55b9675af1ca6bd0752f85888e3
nirvanaemploye <nirvanaemploye@gmail.com>
2025-04-24 10:04:58 +0530

    change first letter upper

--- app/(pages)/stories/page.tsx
+++ app/(pages)/stories/page.tsx
@@ -5,7 +5,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useTheme } from "@/app/components/context/themeContext";
 
-const page = () => {
+const Page = () => {
 
   const { theme } = useTheme();
 
@@ -64,4 +64,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
